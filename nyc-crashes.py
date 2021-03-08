98024e017a3b279a94d099dd5834eb4a3b4b49a1
Regis Schulze <schulze_regis@hotmail.com>
2021-03-08 15:53:39 +0100

    data head

---
+++ nyc-crashes.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+filename="/home/regis/Desktop/machine learning/ANT-Theano-2-27-main (1) (1)/ANT-Theano-2-27-main/additional_resources/datasets/NYC Motor Vehicle Crashes/data_100000.csv"
+df = pd.read_csv(filename)
+print(df.head())
